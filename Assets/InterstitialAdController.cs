93bf39ab955f41f68a61c166a60a7f4b0347a11a
artisan7 <jgbation4@gmail.com>
2022-11-11 22:42:39 +0800

    Added Interstitial Ads

---
+++ Assets/InterstitialAdController.cs
@@ -0,0 +1,76 @@
+using System;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class InterstitialAdController : MonoBehaviour
+{
+    // This ad unit is configured to always serve test banner ads for android.
+    private string androidAppId = "ca-app-pub-3212738706492790/6113697308";
+    private InterstitialAd interstitialAd;
+
+    private void Start()
+    {
+        // Preload the ad
+        RequestInterstitial();
+    }
+
+    private void RequestInterstitial()
+    {
+        // Clean up previous ad before creating a new one to avoid memory leak
+        if (interstitialAd != null)
+            interstitialAd.Destroy();
+
+        // Initialize interstitial ad
+        interstitialAd = new InterstitialAd(androidAppId);
+
+        // Register ad events
+        interstitialAd.OnAdLoaded += HandleAdLoaded;
+        interstitialAd.OnAdFailedToLoad += HandleAdFailedToLoad;
+        interstitialAd.OnAdOpening += HandleAdOpening;
+        interstitialAd.OnAdClosed += HandleAdClosed;
+
+        // create empty add request
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // load interstitial ad using request
+        interstitialAd.LoadAd(request);
+    }
+
+    // This function is attached and called when certain buttons are pressed
+    public void HandleShowInterstitial()
+    {
+        if (interstitialAd.IsLoaded())
+            interstitialAd.Show();
+        else
+            Debug.Log("Please wait a few seconds for the Ad to Load.");
+    }
+
+    #region Interstitial callback handlers
+    public void HandleAdLoaded(object sender, EventArgs args)
+    {
+        Debug.Log("Interstitial Ad Loaded");
+    }
+
+    public void HandleAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        Debug.Log("Interstitial Ad Load Failed:" + args.LoadAdError.GetMessage());
+    }
+
+    public void HandleAdOpening(object sender, EventArgs args)
+    {
+        Debug.Log("Interstitial Ad Shown");
+    }
+
+    public void HandleAdClosed(object sender, EventArgs args)
+    {
+        Debug.Log("Interstitial Ad Closed");
+
+        // Clean up ad after closing to avoid memory leak
+        interstitialAd.Destroy();
+
+        // Preload another ad after 3 seconds
+        Invoke("RequestInterstitial", 3);
+    }
+
+    #endregion
+}
