5a326cbbb87ed85ec88158d8012e9936042c9fce
artisan7 <jgbation4@gmail.com>
2022-11-11 18:42:24 +0800

    Switched to Adaptive banners and added buttons to control Banner ads

--- Assets/AdManager.cs
+++ Assets/AdManager.cs
@@ -1,10 +1,10 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 using GoogleMobileAds.Api;
 
 public class AdManager : MonoBehaviour
 {
-    string androidAppID = "ca-app-pub-3940256099942544/6300978111";
-
     private BannerView bannerView;
 
     // Start is called before the first frame update
@@ -13,17 +13,84 @@ public class AdManager : MonoBehaviour
         // Initialize Google Mobile Ads SDK
         MobileAds.Initialize(initStatus => { });
 
-        this.RequestBanner();
+        this.RequestBanner(AdPosition.Top, 100);
     }
 
-    private void RequestBanner()
+    private void RequestBanner(AdPosition position, int width = 0)
     {
-        this.bannerView = new BannerView(androidAppID, AdSize.SmartBanner, AdPosition.Top);
+        // These ad units are configured to always serve test ads.
+        #if UNITY_EDITOR
+            string adUnitId = "unused";
+        #elif UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3212738706492790/6113697308";
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3212738706492790/5381898163";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        // Clean up previous banner before creating a new one to avoid memory leak
+        if (bannerView != null)
+            bannerView.Destroy();
+
+        // create an adaptive size based on screen size
+        //AdSize fullWidthSize = AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(AdSize.FullWidth);
+        
+        // calculate bannerWidth if width parameter is specified
+        AdSize bannerSize = AdSize.GetCurrentOrientationAnchoredAdaptiveBannerAdSizeWithWidth(width);
+        bannerView = new BannerView(adUnitId, bannerSize, position);
+
+        // Register ad events
+        bannerView.OnAdLoaded += HandleAdLoaded;
+        bannerView.OnAdFailedToLoad += HandleAdFailedToLoad;
+        bannerView.OnAdOpening += HandleAdOpening;
+        bannerView.OnAdClosed += HandleAdClosed;
 
         // create empty add request
         AdRequest request = new AdRequest.Builder().Build();
 
         // load banner using request
-        this.bannerView.LoadAd(request);
+        bannerView.LoadAd(request);
+    }
+
+    public void HandleToggleBanner(int position)
+    {
+        // AdPosition values
+        // Top = 0,
+        // Bottom = 1,
+        // TopLeft = 2,
+        // TopRight = 3,
+        // BottomLeft = 4,
+        // BottomRight = 5,
+        // Center = 6
+        if (bannerView != null)
+        {
+            bannerView.Destroy();
+            bannerView = null;
+        }
+        else
+        {
+            RequestBanner((AdPosition)position, 100);
+        }
+    }
+
+    public void HandleAdLoaded(object sender, EventArgs args)
+    {
+        Debug.Log("Ad Loaded");
+    }
+
+    public void HandleAdFailedToLoad(object sender, EventArgs args)
+    {
+        Debug.Log("Ad Load Failed");
+    }
+
+    public void HandleAdOpening(object sender, EventArgs args)
+    {
+        Debug.Log("Ad Clicked");
+    }
+
+    public void HandleAdClosed(object sender, EventArgs args)
+    {
+        Debug.Log("Ad Closed");
     }
 }
